c065c5d1ed23141083feb85ec072ac53b9e1b15c
ND11111 <160196623+ND11111@users.noreply.github.com>
2025-09-21 20:58:49 -0400

    Update LEDDriver.cpp, simple on/off without time duration

--- LEDDriver/LEDDriver.cpp
+++ LEDDriver/LEDDriver.cpp
@@ -9,32 +9,23 @@ LEDDriver::LEDDriver(uint8_t LEDpin){
 void LEDDriver::begin(){
   pinMode(_ledPin, OUTPUT);
   digitalWrite(_ledPin, LOW);  //keep off initially 
-  onTime = 0;
-  Start  = 0;
-  isOn = false; 
 }
 
-void LEDDriver::turnON(uint32_t duration) {
-  if (!isOn) {
-    isOn = true;
-    Start = millis(); //save start time
-    onTime = duration; //save length LED should stay on 
-    digitalWrite(_ledPin, HIGH);
-     if (DEBUG) {
-      Serial.println("LED On");
-     }
+void LEDDriver::on() {
+  digitalWrite(_ledPin, HIGH);
+  if (DEBUG) {
+    Serial.println("LED On");
   }
 }
 
-void LEDDriver::update() {
-  if (isOn && onTime > 0 && (millis()- Start >= onTime)) {
-    isOn = false;
-    digitalWrite(_ledPin, LOW);
-    if (DEBUG) {
-      Serial.println("LED Off");
-     }
+void LEDDriver::off() {
+  digitalWrite(_ledPin, LOW);
+  if (DEBUG) {
+    Serial.println("LED Off");
   }
 }
+
+
 
 
 
