82e55edeff3cc466fe5c3c61baf34469bcc0a972
ND11111 <160196623+ND11111@users.noreply.github.com>
2025-11-06 21:02:29 -0500

    emptied BatteryMonitor.h

--- BatteryMonitor.h
+++ BatteryMonitor.h
@@ -1,17 +1,1 @@
-#ifndef BATTERY_MONITOR_H
-#define BATTERY_MONITOR_H
 
-#include <Arduino.h>
-
-class BatteryMonitor {
-public:
-  BatteryMonitor(int pinNumber);
-  void setPin();           
-  float readVoltage();
-  int readPercent();
-
-private:
-  int pin;
-};
-
-#endif
