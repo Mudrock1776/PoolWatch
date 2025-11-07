f30732e7e189d3f1649203ea47dc929fa9d23f71
ND11111 <160196623+ND11111@users.noreply.github.com>
2025-11-06 21:03:09 -0500

    emptied BatteryMonitor.ino

--- BatteryMonitor.ino
+++ BatteryMonitor.ino
@@ -1,18 +1,1 @@
-#include "BatteryMonitor.h"
 
-BatteryMonitor batteryMonitor(34);  // ADC pin
-
-void setup() {
-  Serial.begin(115200);
-  batteryMonitor.setPin();         
-}
-
-void loop() {
-  float voltage = batteryMonitor.readVoltage();
-  int percent = batteryMonitor.readPercent();
-
-  Serial.printf("Battery: %.2f V\n", voltage);
-  Serial.printf("Percent: %3d%%\n\n", percent);
-
-  delay(5000);
-}
