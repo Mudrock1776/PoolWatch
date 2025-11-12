43b3f388ff3fe387716f16f6674589e4bd10c832
Austin Naugle <142855943+Anaugle@users.noreply.github.com>
2025-11-12 13:00:17 -0500

    Modify chlorine_phosphate_driver class constructor

--- PoolWatchDrivers/ClPhMeasurementDriver/chlorine_phosphate_driver.h
+++ PoolWatchDrivers/ClPhMeasurementDriver/chlorine_phosphate_driver.h
@@ -8,7 +8,7 @@ const uint8_t photoPin_ph = 34;  // predefined pin for phosphate
 class chlorine_phoshpate_driver 
 {
 	public:
-		chlorine_phoshpate_driver(uint8_t Clpin);
+		chlorine_phoshpate_driver(uint8_t Clpin, uint8_t Ppin);
     	void begin(); 
 		void on();
     	void off();
@@ -16,10 +16,12 @@ class chlorine_phoshpate_driver
 		float PConcentration();
   
     private:
-		uint8_t _PDpin;
+		uint8_t _PDClpin;
+		uint8_t _PDPpin;
 		
 };
 extern bool DEBUG;
 
 
 #endif
+
