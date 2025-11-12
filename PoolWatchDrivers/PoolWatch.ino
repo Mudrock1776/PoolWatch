a4ef4102dbf31326447eaa4cdb63536d7ada70c4
ND11111 <160196623+ND11111@users.noreply.github.com>
2025-11-12 13:20:41 -0500

    Update PoolWatch.ino

--- PoolWatchDrivers/PoolWatch.ino
+++ PoolWatchDrivers/PoolWatch.ino
@@ -10,8 +10,8 @@
 char *WIFI_SSID = ""; //Your wifi name
 char *WIFI_PASSWORD = ""; //Your wifi password
 
-char *SERVER_HOST = "device.necrass"; //This would be the hostname of the website
-char *SERVER_IP = "poolswatch.com"; //This is needed for my tests with my home webserver
+char *SERVER_HOST = ""; //This would be the hostname of the website
+char *SERVER_IP = ""; //This is needed for my tests with my home webserver
 int SERVER_PORT = 80;
 int DEVICE_SERIAL = 1;
 unsigned long int StatusDelay = 100000;
@@ -33,86 +33,85 @@ Relay stirrer(RELAY4PIN);
 //Relay On Timings 
 unsigned long pumpMs = 5000;  // 5s
 unsigned long solenoidMs = 2000;  
-unsigned long stirrerMs = 3000;  
-unsigned long phosphateWait = 6000; //wait time b/w phosphate reagent dispensing 
+unsigned long stirrerMs = 5000; //5s
+//wait time b/w phosphate reagent dispensing 
+unsigned long phosphateWait = 120000; //2min 
+unsigned long phosphateWait2 = 60000;//1min
 //Spacing Delays b/w operations
-unsigned long pumpToSolenoidDelay = 1000;  // pause after pump ends for settling 
-unsigned long solenoidToStirDelay = 500;   // pause after solenoid ends
-unsigned long phosphateLoadWait = 45000;  // 45 pause b/w phosphate reagent loading
+unsigned long loadingDelay = 45000;  // 45 pause b/w reagent loading
 
 float statusOutput[5];
 
-//chlorineParticulate Run 
-void runChlorineParticulateSequence() {
-  CuvvettesFull = false;
-  //running pump
+void isCuvvettesFilled() {
+  if (CuvvettesFull) return;  
   pump.turnOnFor(pumpMs);
   while (pump.state() == HIGH) {
     pump.update();
-  }
-  if (DEBUG) {
-    Serial.print("Waiting ");
-  }
-  delay(pumpToSolenoidDelay);
-  //running solenoid 1 
-  solenoid1.turnOnFor(solenoidMs);
-  while (solenoid1.state() == HIGH) {
-    solenoid1.update();
-  }
-  if (DEBUG) {
-    Serial.print("Waiting ");
-  }
-  delay(solenoidToStirDelay);
-  //running stirrer 
-  stirrer.turnOnFor(stirrerMs);
-  while (stirrer.state() == HIGH) {
-    stirrer.update();
-  }
-  CuvvettesFull = true; //end
-}
-
-//phosphate run
-void runPhosphateSequence() {
-  CuvvettesFull = false;
-  // Pump on
-  pump.turnOnFor(pumpMs);
-  while (pump.state() == HIGH) {
-    pump.update();
-  }
-  //wait between pump and first solenoid 
-  if (DEBUG) {
-    Serial.print("Waiting ");
-    Serial.print(pumpToSolenoidDelay);
-  }
-  delay(pumpToSolenoidDelay);
-  //3 cycles of wait then solenoid2 then stir
-  for (int cycle = 0; cycle < 3; cycle++) {
-    //wait to load reagents before this dose
-    if (phosphateWait > 0) {
-      if (DEBUG) {
-      Serial.print("Waiting before cycle ");
-      Serial.print(cycle + 1);
-      Serial.println("...");
-      }
-      unsigned long startWait = millis();
-      while (millis() - startWait < phosphateWait) {
-        delay(10);  
-      }
-    }
-    //run solenoid 2
-    solenoid2.turnOnFor(solenoidMs);
-    while (solenoid2.state() == HIGH) {
-      solenoid2.update();
-    }
-    //run stirrer
-    stirrer.turnOnFor(stirrerMs);
-    while (stirrer.state() == HIGH) {
-      stirrer.update();
-    }
   }
   CuvvettesFull = true;
 }
 
+
+//chlorineParticulate Run 
+void runChlorineSequence() {
+  isCuvvettesFilled();
+  //delay to load prefilled reagents
+  if (loadingDelay > 0) {
+    unsigned long startWait = millis();
+    while (millis() - startWait < loadingDelay) { 
+      delay(10); 
+    }
+  }
+  solenoid1.turnOnFor(solenoidMs);
+  while (solenoid1.state() == HIGH) {
+    solenoid1.update();
+  }
+  stirrer.turnOnFor(stirrerMs);
+  while (stirrer.state() == HIGH) {
+    stirrer.update();
+  }
+}
+
+//phosphate run
+void runPhosphateSequence() {
+  isCuvvettesFilled();
+  if (loadingDelay > 0) {
+    unsigned long startWait = millis();
+    while (millis() - startWait < loadingDelay) { 
+      delay(10); 
+    }
+  }
+  //run solenoid 2 for Reagent 1
+  solenoid2.turnOnFor(solenoidMs);
+  while (solenoid2.state() == HIGH) {
+    solenoid2.update();
+  }
+  stirrer.turnOnFor(stirrerMs);
+  while (stirrer.state() == HIGH) {
+    stirrer.update();
+  }
+  //Wait 2 min b/f adding next reagent 
+  if (phosphateWait > 0) {
+    unsigned long startWait = millis();
+    while (millis() - startWait < phosphateWait) {
+      delay(10);
+    }
+  }
+  solenoid2.turnOnFor(solenoidMs);
+  while (solenoid2.state() == HIGH) {
+    solenoid2.update();
+  }
+  stirrer.turnOnFor(stirrerMs);
+  while (stirrer.state() == HIGH) {
+    stirrer.update();
+  }
+  if (phosphateWait2 > 0) {
+    unsigned long startWait2 = millis();
+    while (millis() - startWait2 < phosphateWait2) {
+      delay(10);
+    }
+  }
+}
 
 void setup() {
   if (DEBUG){
@@ -123,6 +122,10 @@ void setup() {
   PoolWatchWebDrivers.establishDevice(SERVER_HOST,SERVER_IP,SERVER_PORT,DEVICE_SERIAL);
   batteryMonitor.setPin();
   debugPanelDrivers.initializePins();
+  pump.setPins(); 
+  stirrer.setPins(); 
+  solenoid1.setPins();
+  solenoid2.setPins(); 
   tempSensor.begin();
   CuvvettesFull = false;
 }
@@ -130,17 +133,19 @@ void setup() {
 void loop() {
   float batteryCharge = batteryMonitor.readPercent();
   //bool pumpStatus = true; //If we have away to know this update this value;
-  bool pumpStatus = (pump.state() == HIGH);   // relay HIGH = pump ON
+  bool pumpStatus = (pump.state() == HIGH);   //pump ON
   bool fiveRegulator = debugPanelDrivers.get5RegStatus();
   bool twelveRegulator = debugPanelDrivers.get12RegStatus();
 
   PoolWatchWebDrivers.sendStatus(batteryCharge, true, false, true, statusOutput);
   if(statusOutput[0] != 0){
     if (statusOutput[1]){
+      runChlorineSequence();
       //Run Chlorine Test
       CLCon = 0;
     }
     if (statusOutput[2]){
+      runPhosphateSequence();
       //Run Phosphate Test
       PCon = 0;
     }
@@ -150,12 +155,11 @@ void loop() {
     }
     if (statusOutput[4]){
       //Run Particulate Test
+      isCuvvettesFilled(); 
       PartAmount = 0;
       PartSize = "small";
     }
     PoolWatchWebDrivers.sendReport(tempF, CLCon, PCon, PartAmount, PartSize);
   }
   delay(StatusDelay);
-
-
 }
