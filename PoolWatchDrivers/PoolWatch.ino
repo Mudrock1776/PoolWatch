0fb1b0efd88da39388ae864aa50b141e8d8685b0
ND11111 <160196623+ND11111@users.noreply.github.com>
2025-11-13 11:05:40 -0500

    Update PoolWatch.ino
    Add Debugging prints for sequence

--- PoolWatchDrivers/PoolWatch.ino
+++ PoolWatchDrivers/PoolWatch.ino
@@ -17,6 +17,7 @@ int DEVICE_SERIAL = 1;
 unsigned long int StatusDelay = 100000;
 bool DEBUG = true;
 bool CuvvettesFull;
+bool pumpEverRan = false; // has pump ever run since boot/reset
 float tempF = 0;
 float CLCon = 0;
 float PCon = 0;
@@ -33,20 +34,27 @@ Relay stirrer(RELAY4PIN);
 //Relay On Timings 
 unsigned long pumpMs = 5000;  // 5s
 unsigned long solenoidMs = 2000;  
-unsigned long stirrerMs = 5000; //5s
+unsigned long stirrerMs = 5000; 
 //wait time b/w phosphate reagent dispensing 
 unsigned long phosphateWait = 120000; //2min 
 unsigned long phosphateWait2 = 60000;//1min
 //Spacing Delays b/w operations
-unsigned long loadingDelay = 45000;  // 45 pause b/w reagent loading
+unsigned long loadingDelay = 15000;  // 15 pause b/w reagent loading
 
 float statusOutput[5];
 
 void isCuvvettesFilled() {
-  if (CuvvettesFull) return;  
+  if (CuvvettesFull) return;
+  pumpEverRan = true;   //pump ran
+  if (DEBUG) {
+    Serial.println("Pump ON (fill cuvettes)");
+  }
   pump.turnOnFor(pumpMs);
   while (pump.state() == HIGH) {
     pump.update();
+  }
+  if (DEBUG) {
+    Serial.println("Pump OFF");
   }
   CuvvettesFull = true;
 }
@@ -54,63 +62,121 @@ void isCuvvettesFilled() {
 
 //chlorineParticulate Run 
 void runChlorineSequence() {
+  if (DEBUG) {
+    Serial.println("=== Running Chlorine Sequence ===");
+  }
   isCuvvettesFilled();
   //delay to load prefilled reagents
   if (loadingDelay > 0) {
+    if (DEBUG) {
+    Serial.println("Loading delay before chlorine reagent...");
+  }
     unsigned long startWait = millis();
     while (millis() - startWait < loadingDelay) { 
       delay(10); 
     }
+  }
+  if (DEBUG) {
+    Serial.println("Solenoid 1 ON for chlorine reagents");
   }
   solenoid1.turnOnFor(solenoidMs);
   while (solenoid1.state() == HIGH) {
     solenoid1.update();
   }
+  if (DEBUG) {
+    Serial.println("Solenoid 1 OFF");
+  }
+  if (DEBUG) {
+    Serial.println("Stirrer ON");
+  }
   stirrer.turnOnFor(stirrerMs);
   while (stirrer.state() == HIGH) {
     stirrer.update();
+  }
+  if (DEBUG) {
+  Serial.println("Stirrer OFF");
+  Serial.println("=== Chlorine Sequence Done ===");
   }
 }
 
 //phosphate run
 void runPhosphateSequence() {
+  if (DEBUG) {
+    Serial.println("=== Running Phosphate Sequence ===");
+  }
   isCuvvettesFilled();
   if (loadingDelay > 0) {
+    if (DEBUG) {
+      Serial.println("Loading delay before first phosphate reagent...");
+  }
     unsigned long startWait = millis();
     while (millis() - startWait < loadingDelay) { 
       delay(10); 
     }
   }
+    if (DEBUG) {
+      Serial.println("Solenoid 2 ON (phosphate reagent 1)");
+    }
   //run solenoid 2 for Reagent 1
   solenoid2.turnOnFor(solenoidMs);
   while (solenoid2.state() == HIGH) {
     solenoid2.update();
   }
+    if (DEBUG) {
+      Serial.println("Solenoid 2 OFF");
+      Serial.println("Stirrer ON");
+    }
   stirrer.turnOnFor(stirrerMs);
   while (stirrer.state() == HIGH) {
     stirrer.update();
   }
+    if (DEBUG) {
+      Serial.println("Stirrer OFF");
+    }
   //Wait 2 min b/f adding next reagent 
   if (phosphateWait > 0) {
-    unsigned long startWait = millis();
+    if (DEBUG) {
+      Serial.println("Waiting 2 min before second phosphate reagent...");
+    }
+      unsigned long startWait = millis();
     while (millis() - startWait < phosphateWait) {
       delay(10);
     }
   }
+    if (DEBUG) {
+      Serial.println("Solenoid 2 ON (phosphate reagent 2)");
+    }
   solenoid2.turnOnFor(solenoidMs);
   while (solenoid2.state() == HIGH) {
     solenoid2.update();
   }
+    if (DEBUG) {
+      Serial.println("Solenoid 2 OFF");
+      Serial.println("Stirrer ON for second mix");
+    }
   stirrer.turnOnFor(stirrerMs);
   while (stirrer.state() == HIGH) {
     stirrer.update();
   }
+    if (DEBUG) {
+      Serial.println("Stirrer OFF");
+    }
   if (phosphateWait2 > 0) {
+    if (DEBUG) {
+      Serial.println("Final wait after phosphate sequence...");
+    }
     unsigned long startWait2 = millis();
     while (millis() - startWait2 < phosphateWait2) {
       delay(10);
     }
   }
+  if (DEBUG) {
+      Serial.println("=== Phosphate Sequence Done ===");
+  }
+}
+
+bool getPumpHealth() {
+    return pumpEverRan && debugPanelDrivers.get12RegStatus();
 }
 
 void setup() {
@@ -133,9 +199,9 @@ void setup() {
 void loop() {
   float batteryCharge = batteryMonitor.readPercent();
   //bool pumpStatus = true; //If we have away to know this update this value;
-  bool pumpStatus = (pump.state() == HIGH);   //pump ON
   bool fiveRegulator = debugPanelDrivers.get5RegStatus();
   bool twelveRegulator = debugPanelDrivers.get12RegStatus();
+  bool pumpStatus = getPumpHealth();//tie to both 12V regulator being ok and if pump ever ran
 
   PoolWatchWebDrivers.sendStatus(batteryCharge, true, false, true, statusOutput);
   if(statusOutput[0] != 0){
